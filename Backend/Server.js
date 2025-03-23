47eb6132f5237c9c76ca593cecd3b01c3127d99a
suranjay kumar chandrawanshi <cssuranjaykumar@gmail.com>
2025-03-23 00:06:42 -0700

    Delete Backend directory

--- Backend/Server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/app'); // Use quotes around the path
-require('dotenv').config();
-
-app.listen(3000, () => {
-    console.log("server is running");
-});
--- Backend/src/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const aiController = require("../controllers/ai.controller");
-
-const router = express.Router(); // Corrected the variable name
-
-router.get('/get-review', aiController.getReview );
-
-module.exports = router;
